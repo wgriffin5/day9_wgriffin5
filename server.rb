0d97023e370ab3b2353b12c814fb23560a843898
William Griffin <williamgriffin989@gmail.com>
2015-01-19 20:37:58 -0500

    filling

--- server.rb
+++ server.rb
@@ -3,3 +3,75 @@ require 'sinatra'
 get '/' do 
 	"hello"
 end
+
+DataMapper.setup(
+	:default,
+	'mysql://root@localhost/zzzzzzzz'
+
+	)
+
+
+class Blog
+	include DataMapper: :Resource
+	property :title, String
+	property :content, String
+
+end 
+
+get '/' do 
+	@blogs = Blog.all
+	erb :index
+end
+
+
+# New
+get '/new' do
+	erb :create_blog_form
+
+
+#Create 
+
+post '/create_blog' do 
+	p params
+	@title = Blog.new
+	@
+zzzzzzzz
+
+
+# SHOW
+
+get '/horse/:title' do
+	@blog = Blog.get params[:title]
+end
+
+#  Delete
+
+delete '/delete_blog/:title' do 
+	@blog = Blog.get params[:title]
+	@blog.destroy
+	redirect to '/'
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
